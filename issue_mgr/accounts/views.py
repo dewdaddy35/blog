8f68e376e02fd29b216ac629f5db83a26cf8eafc
Eric Wells <eric.wells41@gmail.com>
2024-02-01 16:03:10 -0800

    Updated Settings

--- issue_mgr/accounts/views.py
+++
@@ -1,9 +0,0 @@
-from django.views.generic import CreateView
-from .forms import CustomUserCreationForm
-from django.urls import reverse_lazy
-
-class SignUpView(CreateView):
-    template_name = "registartion/signup.html"
-    form_class = CustomUserCreationForm
-    success_url = reverse_lazy("login")
-    
